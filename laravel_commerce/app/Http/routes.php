b9497518b51445381a16a611a20d4b708a03dd97
Marlon Andrel Coelho Freitas <marlonmacf@gmail.com>
2015-08-30 19:20:23 -0300

    Image Upload
    Adiciona suporte a upload de imagens para os produtos.

--- laravel_commerce/app/Http/routes.php
+++ laravel_commerce/app/Http/routes.php
@@ -15,23 +15,49 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('auth/login', ['as' => 'login', 'uses' => function () {
+    return view('auth\login');
+}]);
+
+Route::get('auth/register', ['as' => 'register', 'uses' => function () {
+    return view('auth\register');
+}]);
+
 Route::group(['prefix' => 'admin', 'where' => ['id' => '[0-9]+']], function() {
 
     Route::group(['prefix'=>'categories'], function() {
+
         Route::get('/', ['as' => 'categories', 'uses' => 'AdminCategoriesController@index']);
         Route::post('/', ['as' => 'categories.store', 'uses' => 'AdminCategoriesController@store']);
         Route::get('create', ['as' => 'categories.create', 'uses' => 'AdminCategoriesController@create']);
-        Route::get('{id}/destroy', ['as' => 'categories.destroy', 'uses' => 'AdminCategoriesController@destroy']);
-        Route::get('{id}/edit', ['as' => 'categories.edit', 'uses' => 'AdminCategoriesController@edit']);
-        Route::put('{id}/update', ['as' => 'categories.update', 'uses' => 'AdminCategoriesController@update']);
+
+        Route::group(['prefix' => '{id}'], function () {
+
+            Route::get('destroy', ['as' => 'categories.destroy', 'uses' => 'AdminCategoriesController@destroy']);
+            Route::get('edit', ['as' => 'categories.edit', 'uses' => 'AdminCategoriesController@edit']);
+            Route::put('update', ['as' => 'categories.update', 'uses' => 'AdminCategoriesController@update']);
+        });
     });
 
     Route::group(['prefix' => 'products'], function () {
+
         Route::get('/', ['as' => 'products', 'uses' => 'AdminProductsController@index']);
         Route::post('/', ['as' => 'products.store', 'uses' => 'AdminProductsController@store']);
         Route::get('create', ['as' => 'products.create', 'uses' => 'AdminProductsController@create']);
-        Route::get('{id}/destroy', ['as' => 'products.destroy', 'uses' => 'AdminProductsController@destroy']);
-        Route::get('{id}/edit', ['as' => 'products.edit', 'uses' => 'AdminProductsController@edit']);
-        Route::put('{id}/update', ['as' => 'products.update', 'uses' => 'AdminProductsController@update']);
+
+        Route::group(['prefix' => '{id}'], function () {
+
+            Route::get('destroy', ['as' => 'products.destroy', 'uses' => 'AdminProductsController@destroy']);
+            Route::get('edit', ['as' => 'products.edit', 'uses' => 'AdminProductsController@edit']);
+            Route::put('update', ['as' => 'products.update', 'uses' => 'AdminProductsController@update']);
+
+            Route::group(['prefix' => 'images'], function () {
+
+                Route::get('/', ['as' => 'products.images', 'uses' => 'AdminProductsController@images']);
+                Route::get('create', ['as' => 'products.images.create', 'uses' => 'AdminProductsController@createImage']);
+                Route::post('store', ['as' => 'products.images.store', 'uses' => 'AdminProductsController@storeImage']);
+                Route::get('destroy', ['as' => 'products.images.destroy', 'uses' => 'AdminProductsController@destroyImage']);
+            });
+        });
     });
 });
