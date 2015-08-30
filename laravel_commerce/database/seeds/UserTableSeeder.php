d8a927b3550f09837491d11d30243829b452544d
Marlon Andrel Coelho Freitas <marlonmacf@gmail.com>
2015-08-29 21:52:49 -0300

    Seeding
    Nessa fase, foram adicionados dois Fakes nas seguintes entidades: User,
    Category, Product.

---
+++ laravel_commerce/database/seeds/UserTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use CodeCommerce\User;
+//use Faker\Factory as Faker;
+
+class UserTableSeeder extends \Illuminate\Database\Seeder
+{
+
+    public function run()
+    {
+        DB::table('users')->truncate();
+
+        factory('CodeCommerce\User')->create([
+            'name' => 'Marlon',
+            'email' => 'marlonmacf@gmail.com',
+            'password' => Hash::make(123456)
+        ]);
+
+        factory('CodeCommerce\User', 10)->create();
+
+        /*
+        $faker = Faker::create();
+
+        User::create([
+            'name' => 'Marlon',
+            'email' => 'marlonmacf@gmail.com',
+            'password' => Hash::make(123456)
+        ]);
+
+        foreach(range(1,10) as $i) {
+            User::create([
+                'name' => $faker->name(),
+                'email' => $faker->email(),
+                'password' => Hash::make($faker->word())
+            ]);
+        }
+        */
+    }
+}
